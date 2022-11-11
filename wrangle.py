2795e2c4f2b8fbdf94a3528e250699c35a938d2c
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-11-11 17:22:08 -0600

    update wrangle

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,8 @@ import env
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import QuantileTransformer
 
 def get_zillow_data():
         ''' Acquire Zillow data using properties_2017 table from Code up Data Base. Columns bedroomcnt, 
@@ -49,6 +51,52 @@ def wrangle_single_family_residential():
     return df
 
 
+def remove_outliers(df,feature_list):
+    ''' utilizes IQR to remove data which lies beyond 
+    three standard deviations of the mean
+    '''
+    for feature in feature_list:
+    
+        #define interquartile range
+        Q1= df[feature].quantile(0.25)
+        Q3 = df[feature].quantile(0.75)
+        IQR = Q3 - Q1
+        #Set limits
+        upper_limit = Q3 + 3 * IQR
+        lower_limit = Q1 - 2.5 * IQR
+
+        #remove outliers
+        df = df[(df[feature] > lower_limit) & (df[feature] < upper_limit)]
+    
+    return df
+
+def handle_outliers(df):
+    """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
+    df = df[df.bathroom <= 8]
+    
+    df = df[df.bedroom<= 8]
+
+    df = df[df.tax_value < 2_000_000]
+
+    df = df[df.squarefeet < 10000]
+
+    return df
+
+
+def split_data(df):
+    '''
+    split_data takes in data Frame and splits into  train , validate, test.
+    The split is 20% test 80% train/validate. Then 30% of 80% validate and 70% of 80% train.
+    Aproximately (train 56%, validate 24%, test 20%)
+    Returns train, validate, and test 
+    '''
+    # split test data from train/validate
+    train_and_validate, test = train_test_split(df, random_state=123, test_size=.2)
+
+    # split train from validate
+    train, validate = train_test_split(train_and_validate, random_state=123, test_size=.3)
+                                   
+    return train, validate, test
 
 def scale_data(train, 
                validate, 
